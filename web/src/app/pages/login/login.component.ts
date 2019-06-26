c3262d95ddb86a71abec5f860caddd8cf3bc0373
josealcaraz <jose.alcaraz@pragma.com.co>
2019-06-26 11:11:07 -0500

    first post example

--- web/src/app/pages/login/login.component.ts
+++ web/src/app/pages/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './../../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [ LoginService ]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public infoUser:any;
+
+  constructor(
+    private _LoginService:LoginService,
+  ) { }
 
   ngOnInit() {
+    this._LoginService.logUser().subscribe(
+      result => this.infoUser = result,
+      error => console.log("An error has ocurred," + error )
+    )
+
+    console.log(this.infoUser);
   }
+
+  // goToHome() {
+  //   this._router.navigate(['/home']);
+  // }
 
 }
